6121d119c6c26fd0931b2d827bda24ac6029a0fe
Felipe Mejias <felipe.mejias50@gmail.com>
2024-07-10 16:52:52 -0300

    primeiro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { getUsuarios } from './api';
+import styled from 'styled-components';
 
 function App() {
+  const [lista,setLista]=useState([])
+  useEffect(()=>{
+    const promise=getUsuarios()
+    promise.then(res=>{
+      const {data}=res
+      setLista(data)
+    })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tudo>
+        {lista.map(item=><Usu>
+          <p>nome: {item.nome}</p>
+          <p>email: {item.email}</p>
+          <p>senha: {item.senhaCript}</p>
+          <p>icone: {item.icone}</p>
+        </Usu>)}
+      </Tudo>
     </div>
   );
 }
 
 export default App;
+const Tudo=styled.div`
+width:100vw;height:100vh;background-color:green;
+display:flex;flex-direction:column;
+align-items:center;
+`
+const Usu=styled.div`
+width:600px;background-color:white;height:200px;border-radius:10px;
+margin:20px 0 0 0;
+`
